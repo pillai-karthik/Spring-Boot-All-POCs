322ac9ea21d408b1f50e0d224e64c312a2a0a3ab
Karthik Pillai <52003038+karthik261099@users.noreply.github.com>
2022-11-04 14:02:05 +0530

    Spring Boot Initializer

---
+++ src/main/java/com/karthik/allpocs/AllpocsApplication.java
@@ -0,0 +1,13 @@
+package com.karthik.allpocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllpocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllpocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karthik/allpocs/AllpocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthik.allpocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllpocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
